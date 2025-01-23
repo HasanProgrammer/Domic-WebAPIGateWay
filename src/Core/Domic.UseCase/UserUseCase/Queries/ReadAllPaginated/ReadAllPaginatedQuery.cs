d1d250f3cecffe0d6590b813176cc203073fe7b4
Hasan Karami <hasankarami2020313@gmail.com>
2025-01-23 15:06:43 +0330

    update

--- src/Core/Domic.UseCase/UserUseCase/Queries/ReadAllPaginated/ReadAllPaginatedQuery.cs
+++ src/Core/Domic.UseCase/UserUseCase/Queries/ReadAllPaginated/ReadAllPaginatedQuery.cs
@@ -4,7 +4,4 @@ using Domic.Core.UseCase.Contracts.Interfaces;
 
 namespace Domic.UseCase.UserUseCase.Queries.ReadAllPaginated;
 
-public class ReadAllPaginatedQuery : PaginatedQuery, IQuery<ReadAllPaginatedResponse>
-{
-    
-}
+public class ReadAllPaginatedQuery : PaginatedQuery, IQuery<ReadAllPaginatedResponse>;
