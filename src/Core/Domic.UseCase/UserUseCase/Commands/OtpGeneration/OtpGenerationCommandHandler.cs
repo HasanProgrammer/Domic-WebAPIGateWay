10f8a84f43a09f7b9c73ca3bbcd3cfdde3e94a85
Hasan Karami <hasankarami2020313@gmail.com>
2025-01-30 10:02:20 +0330

    update

--- src/Core/Domic.UseCase/UserUseCase/Commands/OtpGeneration/OtpGenerationCommandHandler.cs
+++ src/Core/Domic.UseCase/UserUseCase/Commands/OtpGeneration/OtpGenerationCommandHandler.cs
@@ -1,19 +1,18 @@
 ﻿using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.UseCase.UserUseCase.Contracts.Interfaces;
 using Domic.UseCase.UserUseCase.DTOs.GRPCs.OtpGeneration;
 
 namespace Domic.UseCase.UserUseCase.Commands.OtpGeneration;
 
-public class OtpGenerationCommandHandler : ICommandHandler<OtpGenerationCommand, OtpGenerationResponse>
+public class OtpGenerationCommandHandler(IUserRpcWebRequest userRpcWebRequest) 
+    : ICommandHandler<OtpGenerationCommand, OtpGenerationResponse>
 {
     public Task BeforeHandleAsync(OtpGenerationCommand command, CancellationToken cancellationToken)
         => Task.CompletedTask;
 
-    public Task<OtpGenerationResponse> HandleAsync(OtpGenerationCommand command, 
+    public Task<OtpGenerationResponse> HandleAsync(OtpGenerationCommand command,
         CancellationToken cancellationToken
-    )
-    {
-        throw new NotImplementedException();
-    }
+    ) => userRpcWebRequest.OtpGenerationAsync(command, cancellationToken);
 
     public Task AfterHandleAsync(OtpGenerationCommand command, CancellationToken cancellationToken)
         => Task.CompletedTask;
--- src/Core/Domic.UseCase/UserUseCase/Commands/OtpVerification/OtpVerificationCommandHandler.cs
+++ src/Core/Domic.UseCase/UserUseCase/Commands/OtpVerification/OtpVerificationCommandHandler.cs
@@ -1,19 +1,18 @@
 ﻿using Domic.Core.UseCase.Contracts.Interfaces;
+using Domic.UseCase.UserUseCase.Contracts.Interfaces;
 using Domic.UseCase.UserUseCase.DTOs.GRPCs.OtpVerification;
 
 namespace Domic.UseCase.UserUseCase.Commands.OtpVerification;
 
-public class OtpVerificationCommandHandler : ICommandHandler<OtpVerificationCommand, OtpVerificationResponse>
+public class OtpVerificationCommandHandler(IUserRpcWebRequest userRpcWebRequest) 
+    : ICommandHandler<OtpVerificationCommand, OtpVerificationResponse>
 {
     public Task BeforeHandleAsync(OtpVerificationCommand command, CancellationToken cancellationToken)
         => Task.CompletedTask;
 
-    public Task<OtpVerificationResponse> HandleAsync(OtpVerificationCommand command, 
+    public Task<OtpVerificationResponse> HandleAsync(OtpVerificationCommand command,
         CancellationToken cancellationToken
-    )
-    {
-        throw new NotImplementedException();
-    }
+    ) => userRpcWebRequest.OtpVerificationAsync(command, cancellationToken);
 
     public Task AfterHandleAsync(OtpVerificationCommand command, CancellationToken cancellationToken)
         => Task.CompletedTask;
