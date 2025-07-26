967866c0f23359ba42c20b3adf81956d801c9835
Hasan1375-tech <karami.samdad@gmail.com>
2025-07-26 18:50:46 +0330

    update

--- src/Core/Domic.UseCase/UserUseCase/DTOs/UserDto.cs
+++ src/Core/Domic.UseCase/UserUseCase/DTOs/UserDto.cs
@@ -12,6 +12,7 @@ public class UserDto
     public string PhoneNumber { get; set; }
     public string Email       { get; set; }
     public string Description { get; set; }
+    public bool IsActive      { get; set; }
     
     /*---------------------------------------------------------------*/
     
