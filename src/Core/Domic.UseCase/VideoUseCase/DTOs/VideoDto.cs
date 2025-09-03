a0c41b05d0ee3f5dc2259d23f5845ee6da364566
Hasan1375-tech <karami.samdad@gmail.com>
2025-09-03 18:36:50 +0330

    update

--- src/Core/Domic.UseCase/VideoUseCase/DTOs/VideoDto.cs
+++ src/Core/Domic.UseCase/VideoUseCase/DTOs/VideoDto.cs
@@ -4,6 +4,7 @@ public class VideoDto
 {
     public string Id { get; set; }
     public string TermId { get; set; }
+    public string SeasonId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string VideoUrl { get; set; }
