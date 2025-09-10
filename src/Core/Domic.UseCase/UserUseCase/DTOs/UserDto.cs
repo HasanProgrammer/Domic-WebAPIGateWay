dd3339b36e0e15b5b04f10f9153468d7cce5754a
Hasan Karami <hasankarami2020313@gmail.com>
2025-09-11 00:28:47 +0330

    update

--- src/Core/Domic.UseCase/UserUseCase/DTOs/UserDto.cs
+++ src/Core/Domic.UseCase/UserUseCase/DTOs/UserDto.cs
@@ -6,6 +6,7 @@ namespace Domic.UseCase.UserUseCase.DTOs;
 public class UserDto
 {
     public string Id          { get; set; }
+    public string ImageUrl    { get; set; }
     public string FirstName   { get; set; }
     public string LastName    { get; set; }
     public string Username    { get; set; }
