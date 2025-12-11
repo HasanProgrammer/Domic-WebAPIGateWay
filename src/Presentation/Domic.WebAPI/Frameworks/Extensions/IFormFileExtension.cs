bbd285b7c00b903c635da089f3142b172626b740
Hasan1375-tech <karami.samdad@gmail.com>
2025-12-11 15:28:50 +0330

    update

--- src/Presentation/Domic.WebAPI/Frameworks/Extensions/IFormFileExtension.cs
+++ src/Presentation/Domic.WebAPI/Frameworks/Extensions/IFormFileExtension.cs
@@ -1,7 +1,4 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
-
-namespace Domic.Common.ClassExtensions;
+﻿namespace Domic.Common.ClassExtensions;
 
 public static class IFormFileExtension
 {
@@ -17,101 +14,14 @@ public static class IFormFileExtension
         IWebHostEnvironment webHostEnvironment, bool renameFile = true, CancellationToken cancellationToken = default
     )
     {
-        string uploadPath = default;
         var fileExtension = Path.GetExtension(file.FileName);
         var fileName      = renameFile ? Guid.NewGuid().ToString().Replace("-", "") + fileExtension : file.FileName;
-
-        if (file.IsImage())
-            uploadPath = Path.Combine($"{webHostEnvironment.WebRootPath}", "Storages", "Images", fileName);
-        else if (file.IsVideo())
-            uploadPath = Path.Combine($"{webHostEnvironment.WebRootPath}", "Storages", "Videos", fileName);
+        string uploadPath = Path.Combine($"{webHostEnvironment.WebRootPath}", "Storages", fileName);
         
         await using var fileStream = new FileStream(uploadPath , FileMode.Create);
         
         await file.CopyToAsync(fileStream, cancellationToken);
         
         return ( uploadPath , fileName , fileExtension );
-    }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="file"></param>
-    /// <returns></returns>
-    public static bool IsImage(this IFormFile file)
-    {
-        //Check Extension
-        if
-        (
-            Path.GetExtension(file.FileName).ToLower() != ".jpg"  &&
-            Path.GetExtension(file.FileName).ToLower() != ".jpeg" &&
-            Path.GetExtension(file.FileName).ToLower() != ".png"
-        )
-            return false;
-
-        //Check Readable file & Security
-        var stream = file.OpenReadStream();
-        try
-        {
-            
-            if (!stream.CanRead)
-                return false;
-        
-            byte[] buffer = new byte[(int) file.Length];
-            stream.Read(buffer, 0, (int) file.Length);
-            string content = Encoding.UTF8.GetString(buffer);
-            if (Regex.IsMatch(content, @"<script|<html|<head|<title|<body|<pre|<table|<a\s+href|<img|<plaintext|<cross\-domain\-policy", RegexOptions.IgnoreCase))
-                return false;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-        finally
-        {
-            stream.Close();
-        }
-
-        return true;
-    }
-        
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="file"></param>
-    /// <returns></returns>
-    public static bool IsVideo(this IFormFile file)
-    {
-        //Check Extension
-        if
-        (
-            Path.GetExtension(file.FileName).ToLower() != ".mp4" &&
-            Path.GetExtension(file.FileName).ToLower() != ".avi"
-        )
-            return false;
-
-        //Check Readable file & Security
-        var stream = file.OpenReadStream();
-        try
-        {
-            if (!stream.CanRead)
-                return false;
-        
-            byte[] buffer = new byte[(int) file.Length];
-            stream.Read(buffer, 0, (int) file.Length);
-            string content = Encoding.UTF8.GetString(buffer);
-            if (Regex.IsMatch(content, @"<script|<html|<head|<title|<body|<pre|<table|<a\s+href|<img|<plaintext|<cross\-domain\-policy", RegexOptions.IgnoreCase))
-                return false;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-        finally
-        {
-            stream.Close();
-        }
-
-        return true;
     }
 }
