db660ec629a7c6b3cc4842cca264e76ab365a08d
Hasan1375-tech <karami.samdad@gmail.com>
2025-12-19 16:00:35 +0330

    update

--- src/Presentation/Domic.WebAPI/EntryPoints/HTTPs/Home/V1/StorageController.cs
+++ src/Presentation/Domic.WebAPI/EntryPoints/HTTPs/Home/V1/StorageController.cs
@@ -1,9 +1,12 @@
-﻿using Domic.Core.Common.ClassExtensions;
+﻿#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+using Domic.Core.Common.ClassExtensions;
 using Domic.WebAPI.Frameworks.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-using Route = Domic.Common.ClassConsts.Route;
+using Route   = Domic.Common.ClassConsts.Route;
+using ILogger = Domic.Core.UseCase.Contracts.Interfaces.ILogger;
 
 namespace Domic.WebAPI.EntryPoints.HTTPs.Home.V1;
 
@@ -11,8 +14,9 @@ namespace Domic.WebAPI.EntryPoints.HTTPs.Home.V1;
 [ApiExplorerSettings(GroupName = "Home/Storage")]
 [Route($"{Route.BaseHomeUrl}{Route.BaseStorageUrl}")]
 [ApiVersion("1.0")]
-public class StorageController(IConfiguration configuration, IWebHostEnvironment hostEnvironment) 
-    : ControllerBase
+public class StorageController(IConfiguration configuration, IWebHostEnvironment hostEnvironment,
+    ILogger logger
+) : ControllerBase
 {
     /// <summary>
     /// 
@@ -21,13 +25,29 @@ public class StorageController(IConfiguration configuration, IWebHostEnvironment
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     [HttpPost(Route.UploadStorageUrl)]
-    public async Task<IActionResult> Upload(IFormFile file, CancellationToken cancellationToken)
+    public async Task<IActionResult> Upload([FromQuery] string fileName, CancellationToken cancellationToken)
     {
-        var uploadResult = await file.UploadAsync(hostEnvironment, cancellationToken: cancellationToken);
+        //var uploadResult = await file.UploadAsync(hostEnvironment, cancellationToken: cancellationToken);
+        
+        var extension = Path.GetExtension(fileName);
+        var newFileName = Guid.NewGuid().ToString().Replace("-", "") + extension;
 
+        string filePath = default;
+
+        if ( new[] { ".png", ".jpg", ".jpeg" }.Contains(extension.ToLower()) )
+            filePath = Path.Combine(hostEnvironment.ContentRootPath ?? "", "Storages", "Images", newFileName);
+        else if( new[] { ".mp4", ".avi" }.Contains(extension.ToLower()) )
+            filePath = Path.Combine(hostEnvironment.ContentRootPath ?? "", "Storages", "Videos", newFileName);
+
+        await using var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None, 1024*1024, true);
+        
+        await Request.Body?.CopyToAsync(stream, cancellationToken);
+
+        logger.RecordAsync(Guid.NewGuid().ToString(), "WebAPIGateWay", filePath, cancellationToken);
+        
         var uploadPath = hostEnvironment.IsDevelopment()
-            ? uploadResult.path.Replace("Storages", "")
-            : uploadResult.path.Split("Storages")[1];
+            ? filePath.Replace("Storages", "")
+            : filePath.Split("Storages")[1];
 
         return HttpContext.OkResponse(
             new {
